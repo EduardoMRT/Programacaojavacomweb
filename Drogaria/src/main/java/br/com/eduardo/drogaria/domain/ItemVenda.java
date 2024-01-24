75bb4499e033baf433469927554fdc55f6b58648
EduardoMRT <eduksoficial@gmail.com>
2024-01-24 14:54:23 -0300

    aula 149 - Entidade Venda
    Criação da tabela venda no bd drogaria

---
+++ Drogaria/src/main/java/br/com/eduardo/drogaria/domain/ItemVenda.java
@@ -0,0 +1,61 @@
+package br.com.eduardo.drogaria.domain;
+
+import java.math.BigDecimal;
+
+import javax.persistence.Access;
+import javax.persistence.AccessType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+@SuppressWarnings("serial")
+@Entity
+@Access(AccessType.FIELD)
+public class ItemVenda extends GenericDomain {
+	@Column(nullable = false)
+	private Short quantidade;
+	
+	@Column(nullable = false, precision = 7, scale = 2)
+	private BigDecimal precoParcial;
+	
+	@ManyToOne
+	@JoinColumn(nullable = false)
+	private Produto produto;
+	
+	@ManyToOne
+	@JoinColumn(nullable = false)
+	private Funcionario funcionario;
+
+	public Short getQuantidade() {
+		return quantidade;
+	}
+
+	public void setQuantidade(Short quantidade) {
+		this.quantidade = quantidade;
+	}
+
+	public BigDecimal getPrecoParcial() {
+		return precoParcial;
+	}
+
+	public void setPrecoParcial(BigDecimal precoParcial) {
+		this.precoParcial = precoParcial;
+	}
+
+	public Produto getProduto() {
+		return produto;
+	}
+
+	public void setProduto(Produto produto) {
+		this.produto = produto;
+	}
+
+	public Funcionario getFuncionario() {
+		return funcionario;
+	}
+
+	public void setFuncionario(Funcionario funcionario) {
+		this.funcionario = funcionario;
+	}
+}
