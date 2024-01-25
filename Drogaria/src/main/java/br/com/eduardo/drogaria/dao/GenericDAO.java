5830f2fd9a183affddbb6d85fcd8169e6218fc94
EduardoMRT <eduksoficial@gmail.com>
2024-01-25 08:34:18 -0300

    Aula 153 - EstadoDAO Listar
    Criação do EstadoDAO Listar( ), criação de novos estados no banco de dados, impressão dos dados com for e .size( )

--- Drogaria/src/main/java/br/com/eduardo/drogaria/dao/GenericDAO.java
+++ Drogaria/src/main/java/br/com/eduardo/drogaria/dao/GenericDAO.java
@@ -1,11 +1,22 @@
 package br.com.eduardo.drogaria.dao;
 
+import java.lang.reflect.ParameterizedType;
+import java.util.List;
+
+import org.hibernate.Criteria;
 import org.hibernate.Session;
 import org.hibernate.Transaction;
 
 import br.com.eduardo.drogaria.util.HibernateUtil;
 
 public class GenericDAO<Entidade> {
+	private Class<Entidade> classe;
+	
+	@SuppressWarnings("unchecked")
+	public GenericDAO() {
+		this.classe = (Class<Entidade>) ((ParameterizedType) getClass().getGenericSuperclass()).getActualTypeArguments()[0];
+	} 
+	
 	public void salvar(Entidade entidade) {
 		Session sessao = HibernateUtil.getFabricaDeSessoes().openSession();
 		Transaction transacao = null;
@@ -20,6 +31,19 @@ public class GenericDAO<Entidade> {
 			}
 			throw erro;
 		} finally{
+			sessao.close();
+		}
+	}
+	@SuppressWarnings("unchecked")
+	public List<Entidade> listar(){
+		Session sessao = HibernateUtil.getFabricaDeSessoes().openSession();
+		try{
+			Criteria consulta = sessao.createCriteria(classe);
+			List<Entidade> resultado = consulta.list();
+			return resultado;
+		}catch(RuntimeException erro) {
+			throw erro;
+		}finally {
 			sessao.close();
 		}
 	}
--- Drogaria/src/test/java/br/com/eduardo/drogaria/dao/EstadoDAOTest.java
+++ Drogaria/src/test/java/br/com/eduardo/drogaria/dao/EstadoDAOTest.java
@@ -1,17 +1,37 @@
 package br.com.eduardo.drogaria.dao;
 
+import java.util.List;
+
+import org.junit.Ignore;
 import org.junit.Test;
 
 import br.com.eduardo.drogaria.domain.Estado;
 
 public class EstadoDAOTest {
 	@Test
+	@Ignore //serve para ignorar o salvar()
 	public void salvar() {
 		Estado estado = new Estado();
-		estado.setNome("São Paulo");
-		estado.setSigla("SP");
+		estado.setNome("Rio Grande do Sul");
+		estado.setSigla("RS");
 		
 		EstadoDAO estadoDAO = new EstadoDAO();
 		estadoDAO.salvar(estado);
 	}
+	
+	@Test
+	public void listar() {
+		EstadoDAO estadoDAO = new EstadoDAO();
+		List<Estado> resultado = estadoDAO.listar();
+	
+		//Retorna a quantidade de registros
+		System.out.println("Total de Registros Encontrados: " + resultado.size());
+		
+		/*for:, percorre a lista 1 por 1 jogando, o Estado é um cursor
+		 * então ele recebe o primeiro, segundo... até terminar*/
+		for(Estado estado : resultado) {
+			System.out.println(estado.getCodigo()+" - "+estado.getSigla() + " - " + estado.getNome());
+		}
+	}
+	
 }
