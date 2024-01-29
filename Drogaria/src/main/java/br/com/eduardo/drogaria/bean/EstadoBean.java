ca74d5dddaef613fb37b2fb4d309b9f83e2c18a6
EduardoMRT <eduksoficial@gmail.com>
2024-01-29 09:19:27 -0300

    Utilização do FacesMessage
    Uso do FacesMessage para exibição de mensagem na tela, com p:growl

--- Drogaria/src/main/java/br/com/eduardo/drogaria/bean/EstadoBean.java
+++ Drogaria/src/main/java/br/com/eduardo/drogaria/bean/EstadoBean.java
@@ -1,11 +1,17 @@
 package br.com.eduardo.drogaria.bean;
 
+import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
+import javax.faces.context.FacesContext;
 
 @ManagedBean
 public class EstadoBean {
 	public void salvar() {
-		System.out.println("Programação Web com Java");
+		String texto = "Programação Web com Java";
+		FacesMessage mensagem = new FacesMessage(FacesMessage.SEVERITY_ERROR, texto, texto); //tipo do erro, msg resumida e msg detalhada
+	
+		FacesContext contexto = FacesContext.getCurrentInstance();
+		contexto.addMessage(null, mensagem); //Primeiro parametro sempre é nulo, e o segundo é a mensagem
 	}
 }
 
