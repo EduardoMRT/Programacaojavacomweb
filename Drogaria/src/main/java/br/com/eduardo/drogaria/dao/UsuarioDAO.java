b71b217cbf65cef95fe754ed20c1d46040de06fe
EduardoMRT <eduksoficial@gmail.com>
2024-02-05 12:11:39 -0300

    aula 215 - UsuarioBean - Salvar

---
+++ Drogaria/src/main/java/br/com/eduardo/drogaria/dao/UsuarioDAO.java
@@ -0,0 +1,7 @@
+package br.com.eduardo.drogaria.dao;
+
+import br.com.eduardo.drogaria.domain.Usuario;
+
+public class UsuarioDAO extends GenericDAO<Usuario> {
+
+}
--- Drogaria/src/main/java/br/com/eduardo/drogaria/domain/Usuario.java
+++ Drogaria/src/main/java/br/com/eduardo/drogaria/domain/Usuario.java
@@ -6,6 +6,7 @@ import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.JoinColumn;
 import javax.persistence.OneToOne;
+import javax.persistence.Transient;
 
 @SuppressWarnings("serial")
 @Entity
@@ -35,11 +36,35 @@ public class Usuario extends GenericDomain{
 	public Character getTipo() {
 		return tipo;
 	}
-
+	
+	@Transient //diz que esse método só serve para formatação
+	public String getTipoFormatado(){
+		String tipoFormatado = null;
+			if(tipo == 'A') {
+				tipoFormatado = "Administrador";
+			}else if(tipo == 'B') {
+				tipoFormatado = "Balconista";
+			}else if(tipo == 'G'){
+				tipoFormatado = "Gerente";
+			}
+			
+			return tipoFormatado;
+	}
+	
 	public void setTipo(Character tipo) {
 		this.tipo = tipo;
 	}
-
+	
+	public String getAtivoFormatado() {
+		String ativoFormatado = null;
+		if(ativo == true) {
+			ativoFormatado = "Sim";
+		}else {
+			ativoFormatado = "Não";
+		}
+		return ativoFormatado;
+	}
+	
 	public Boolean getAtivo() {
 		return ativo;
 	}
