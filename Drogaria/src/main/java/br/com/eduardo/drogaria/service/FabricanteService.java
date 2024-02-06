5dc7d1e8ea8412c63a057780bf1a73b4afd3ca84
EduardoMRT <eduksoficial@gmail.com>
2024-02-06 10:54:31 -0300

    aulas 221 a 223
    FabricanteService - Salvar, Editar, Excluir.

--- Drogaria/src/main/java/br/com/eduardo/drogaria/service/FabricanteService.java
+++ Drogaria/src/main/java/br/com/eduardo/drogaria/service/FabricanteService.java
@@ -3,6 +3,8 @@ package br.com.eduardo.drogaria.service;
 import java.util.List;
 
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 
@@ -38,5 +40,29 @@ public class FabricanteService {
 		String json = gson.toJson(fabricante);
 		
 		return json;
+		}
+		
+		//http://127.0.0.1:8080/Drogaria/rest/fabricante
+		@POST
+		public String salvar(String json) {
+			Gson gson = new Gson(); //serve tanto para converter string pra obj quanto obj pra string
+			Fabricante fabricante = gson.fromJson(json, Fabricante.class);
+			FabricanteDAO fabricanteDAO = new FabricanteDAO();
+			fabricanteDAO.salvar(fabricante);
+			
+			String jsonSaida = gson.toJson(fabricante);
+			return jsonSaida;
+		}
+		
+		//http://127.0.0.1:8080/Drogaria/rest/fabricante
+		@PUT
+		public String editar(String json) {
+			Gson gson = new Gson(); //serve tanto para converter string pra obj quanto obj pra string
+			Fabricante fabricante = gson.fromJson(json, Fabricante.class);
+			FabricanteDAO fabricanteDAO = new FabricanteDAO();
+			fabricanteDAO.editar(fabricante);
+			
+			String jsonSaida = gson.toJson(fabricante);
+			return jsonSaida;
 		}
 }
