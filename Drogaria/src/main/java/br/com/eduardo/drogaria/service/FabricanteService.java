719b0eb10616db639ebeca912f3ac444b50e3ac9
EduardoMRT <eduksoficial@gmail.com>
2024-02-06 09:06:44 -0300

    aula 219 - FabricanteService - Buscar
    RestFull

--- Drogaria/src/main/java/br/com/eduardo/drogaria/service/FabricanteService.java
+++ Drogaria/src/main/java/br/com/eduardo/drogaria/service/FabricanteService.java
@@ -4,15 +4,17 @@ import java.util.List;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 
 import com.google.gson.Gson;
 
 import br.com.eduardo.drogaria.dao.FabricanteDAO;
 import br.com.eduardo.drogaria.domain.Fabricante;
 
-//http://127.0.0.1:8080/Drogaria/rest/fabricante
+
 @Path("fabricante")
 public class FabricanteService {
+	//http://127.0.0.1:8080/Drogaria/rest/fabricante
 	@GET
 	public String listar() {
 		FabricanteDAO fabricanteDAO = new FabricanteDAO();
@@ -23,4 +25,18 @@ public class FabricanteService {
 		
 		return json;
 	}
+		
+		//http://127.0.0.1:8080/Drogaria/rest/fabricante/10
+		//cada vez que coloca-se um patch é como se colocasse uma barra
+		@GET
+		@Path("{codigo}")
+		public String buscar(@PathParam("codigo") Long codigo) {
+		FabricanteDAO fabricanteDAO = new FabricanteDAO();
+		Fabricante fabricante = fabricanteDAO.buscar(codigo);
+	
+		Gson gson = new Gson();
+		String json = gson.toJson(fabricante);
+		
+		return json;
+		}
 }
