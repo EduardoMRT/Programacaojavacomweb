3fa17ebe63380df9600971c269fd397be9401706
EduardoMRT <eduksoficial@gmail.com>
2024-02-14 09:24:58 -0300

    aula 225 - Rest Client - Salvar

--- Drogaria/src/main/java/br/com/eduardo/drogaria/bean/FabricanteBean.java
+++ Drogaria/src/main/java/br/com/eduardo/drogaria/bean/FabricanteBean.java
@@ -11,6 +11,7 @@ import javax.faces.bean.ViewScoped;
 import javax.faces.event.ActionEvent;
 import javax.ws.rs.client.Client;
 import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.client.Entity;
 import javax.ws.rs.client.WebTarget;
 
 import org.hibernate.exception.ConstraintViolationException;
@@ -75,11 +76,27 @@ public class FabricanteBean implements Serializable {
 
 	public void salvar() {
 		try {
-			FabricanteDAO fabricanteDAO = new FabricanteDAO();
-			fabricanteDAO.merge(fabricante);
-
+//			FabricanteDAO fabricanteDAO = new FabricanteDAO();
+//			fabricanteDAO.merge(fabricante);
+//
+//			fabricante = new Fabricante();
+//			fabricantes = fabricanteDAO.listar();
+			
+			Client cliente = ClientBuilder.newClient();
+			WebTarget caminho = cliente.target("http://127.0.0.1:8080/Drogaria/rest/fabricante");
+			
+			Gson gson = new Gson();
+			String json = gson.toJson(fabricante);
+			caminho.request().post(Entity.json(json));
+			
 			fabricante = new Fabricante();
-			fabricantes = fabricanteDAO.listar();
+			json = caminho.request().get(String.class);
+			System.out.println(json);
+			
+			Fabricante[] vetor = gson.fromJson(json, Fabricante[].class);
+		
+			fabricantes = Arrays.asList(vetor);
+			
 			
 			Messages.addGlobalInfo("Fabricante salvo com sucesso!");
 		} catch (RuntimeException erro) {
