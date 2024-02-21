fb04ef6cb10ed46657cfb61b0f0e9d71d2bd3f31
EduardoMRT <eduksoficial@gmail.com>
2024-02-21 15:54:29 -0300

    aula 263 - Busca de Produtos - Parte 2

--- Drogaria/src/main/java/br/com/eduardo/drogaria/bean/ProdutoBean2.java
+++ Drogaria/src/main/java/br/com/eduardo/drogaria/bean/ProdutoBean2.java
@@ -6,24 +6,44 @@ import javax.annotation.ManagedBean;
 import javax.annotation.PostConstruct;
 import javax.faces.bean.ViewScoped;
 
+import org.omnifaces.util.Messages;
+
+import br.com.eduardo.drogaria.dao.ProdutoDAO;
 import br.com.eduardo.drogaria.domain.Produto;
 
 @SuppressWarnings("serial")
 @ManagedBean
 @ViewScoped
-public class ProdutoBean2 implements Serializable{
+public class ProdutoBean2 implements Serializable {
 	private Produto produto;
-	
+
 	public Produto getProduto() {
 		return produto;
 	}
-	
+
 	public void setProduto(Produto produto) {
 		this.produto = produto;
 	}
-	
+
 	@PostConstruct
 	public void novo() {
 		produto = new Produto();
+	}
+
+	public void buscar() {
+		try {
+			ProdutoDAO produtoDAO = new ProdutoDAO();
+			Produto resultado = produtoDAO.buscar(produto.getCodigo());
+
+			if (resultado == null) {
+				Messages.addGlobalWarn("O produto não existe");
+			} else {
+				produto = resultado;
+			}
+
+		} catch (RuntimeException erro) {
+			Messages.addGlobalError("Ocorreu um erro ao tentar buscar o produto");
+			erro.printStackTrace();
+		}
 	}
 }
