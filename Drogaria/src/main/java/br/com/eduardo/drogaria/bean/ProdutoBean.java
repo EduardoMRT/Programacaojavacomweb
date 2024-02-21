23df8003179d5e534829614c50b762445fe178df
EduardoMRT <eduksoficial@gmail.com>
2024-02-21 11:59:18 -0300

    Problema na Filtragem
    Está funcionando porém o getFilter não existe mais. Adotar outra solução.

--- Drogaria/src/main/java/br/com/eduardo/drogaria/bean/ProdutoBean.java
+++ Drogaria/src/main/java/br/com/eduardo/drogaria/bean/ProdutoBean.java
@@ -18,6 +18,7 @@ import javax.persistence.criteria.Path;
 
 import org.omnifaces.util.Faces;
 import org.omnifaces.util.Messages;
+import org.primefaces.component.datatable.DataTable;
 import org.primefaces.event.FileUploadEvent;
 import org.primefaces.model.UploadedFile;
 
@@ -164,11 +165,28 @@ public class ProdutoBean implements Serializable {
 	}
 	
 	public void imprimir() {
-		try {
+		try {								 //idDoFormulario:idComponente
+			DataTable tabela = (DataTable) Faces.getViewRoot().findComponent("formListagem:tabela");
+			Map<String, Object> filtros = tabela.getFilters();
+			String proDescricao = (String) filtros.get("descricao");
+			String fabDescricao = (String) filtros.get("fabricante.descricao");
+			
 			String caminho = Faces.getRealPath("/resources/relatorios/produtos.jasper");
-//C:\Eclipse\WorkSpace\.metadata\.plugins\org.eclipse.wst.server.core\tmp0\wtpwebapps\Drogaria\resources\relatorios
+			//C:\Eclipse\WorkSpace\.metadata\.plugins\org.eclipse.wst.server.core\tmp0\wtpwebapps\Drogaria\resources\relatorios
 			Map<String, Object> parametros = new HashMap<>();
-
+			
+			if(proDescricao == null) {
+				parametros.put("PRODUTO_DESCRICAO", "%%");
+			}else {
+				parametros.put("PRODUTO_DESCRICAO", "%" + proDescricao + "%");
+			}
+			
+			if(fabDescricao == null) {
+				parametros.put("FABRICANTE_DESCRICAO", "%%");
+			}else {
+				parametros.put("FABRICANTE_DESCRICAO", "%" + fabDescricao + "%");
+			}
+			
 			Connection conexao = HibernateUtil.getConexao();
 
 			JasperPrint relatorio = JasperFillManager.fillReport(caminho, parametros, conexao);
