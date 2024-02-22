c3131dea131cbfca6968b920a5073ecac1a1c80d
EduardoMRT <eduksoficial@gmail.com>
2024-02-22 10:49:32 -0300

    aula 266 - Histórico - Salvar - Parte 2

--- Drogaria/src/main/java/br/com/eduardo/drogaria/bean/HistoricoBean.java
+++ Drogaria/src/main/java/br/com/eduardo/drogaria/bean/HistoricoBean.java
@@ -1,7 +1,7 @@
 package br.com.eduardo.drogaria.bean;
 
 import java.io.Serializable;
-
+import java.util.Date;
 
 import javax.annotation.PostConstruct;
 import javax.faces.bean.ManagedBean;
@@ -9,7 +9,9 @@ import javax.faces.bean.ViewScoped;
 
 import org.omnifaces.util.Messages;
 
+import br.com.eduardo.drogaria.dao.HistoricoDAO;
 import br.com.eduardo.drogaria.dao.ProdutoDAO;
+import br.com.eduardo.drogaria.domain.Historico;
 import br.com.eduardo.drogaria.domain.Produto;
 
 @SuppressWarnings("serial")
@@ -18,7 +20,8 @@ import br.com.eduardo.drogaria.domain.Produto;
 public class HistoricoBean implements Serializable {
 	private Produto produto;
 	private Boolean exibePainelDados;
-
+	private Historico historico;
+	
 	public Produto getProduto() {
 		return produto;
 	}
@@ -37,10 +40,19 @@ public class HistoricoBean implements Serializable {
 	
 	@PostConstruct
 	public void novo() {
+		historico = new Historico();
 		produto = new Produto();
 		exibePainelDados = false;
 	}
-
+	
+	public Historico getHistorico() {
+		return historico;
+	}
+	
+	public void setHistorico(Historico historico) {
+		this.historico = historico;
+	}
+	
 	public void buscar() {
 		try {
 			ProdutoDAO produtoDAO = new ProdutoDAO();
@@ -58,6 +70,21 @@ public class HistoricoBean implements Serializable {
 
 		} catch (RuntimeException erro) {
 			Messages.addGlobalError("Ocorreu um erro ao tentar buscar o produto");
+			erro.printStackTrace();
+		}
+	}
+	
+	public void salvar() {
+		try {
+			historico.setHorario(new Date());
+			historico.setProduto(produto);
+			
+			HistoricoDAO historicoDAO = new HistoricoDAO();
+			historicoDAO.salvar(historico);
+			
+			Messages.addGlobalInfo("Histórico salvo com sucesso");
+		} catch (RuntimeException erro) {
+			Messages.addGlobalError("Ocorreu um erro ao tentar salvar o histórico");
 			erro.printStackTrace();
 		}
 	}
