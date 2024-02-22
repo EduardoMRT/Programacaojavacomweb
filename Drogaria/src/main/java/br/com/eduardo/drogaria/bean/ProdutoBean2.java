44d4a5fe6cc5123efa3e0bffbb0e4268bbe30109
EduardoMRT <eduksoficial@gmail.com>
2024-02-22 09:16:54 -0300

    aula 264 - Busca de Produtos - Parte 3

--- Drogaria/src/main/java/br/com/eduardo/drogaria/bean/ProdutoBean2.java
+++ Drogaria/src/main/java/br/com/eduardo/drogaria/bean/ProdutoBean2.java
@@ -2,8 +2,9 @@ package br.com.eduardo.drogaria.bean;
 
 import java.io.Serializable;
 
-import javax.annotation.ManagedBean;
+
 import javax.annotation.PostConstruct;
+import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ViewScoped;
 
 import org.omnifaces.util.Messages;
@@ -16,6 +17,7 @@ import br.com.eduardo.drogaria.domain.Produto;
 @ViewScoped
 public class ProdutoBean2 implements Serializable {
 	private Produto produto;
+	private Boolean exibePainelDados;
 
 	public Produto getProduto() {
 		return produto;
@@ -24,10 +26,19 @@ public class ProdutoBean2 implements Serializable {
 	public void setProduto(Produto produto) {
 		this.produto = produto;
 	}
-
+	
+	public Boolean getExibePainelDados() {
+		return exibePainelDados;
+	}
+	
+	public void setExibePainelDados(Boolean exibePainelDados) {
+		this.exibePainelDados = exibePainelDados;
+	}
+	
 	@PostConstruct
 	public void novo() {
 		produto = new Produto();
+		exibePainelDados = false;
 	}
 
 	public void buscar() {
@@ -36,9 +47,13 @@ public class ProdutoBean2 implements Serializable {
 			Produto resultado = produtoDAO.buscar(produto.getCodigo());
 
 			if (resultado == null) {
+				exibePainelDados = false;
 				Messages.addGlobalWarn("O produto não existe");
+				
 			} else {
+				exibePainelDados = true;
 				produto = resultado;
+				
 			}
 
 		} catch (RuntimeException erro) {
