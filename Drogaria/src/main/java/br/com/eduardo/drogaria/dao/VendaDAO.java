213c4486ccee40c01890fb49e985600961e0ba82
EduardoMRT <eduksoficial@gmail.com>
2024-02-23 10:37:48 -0300

    aula 268 - Baixa no Estoque - Parte 2

--- Drogaria/src/main/java/br/com/eduardo/drogaria/dao/VendaDAO.java
+++ Drogaria/src/main/java/br/com/eduardo/drogaria/dao/VendaDAO.java
@@ -1,4 +1,5 @@
 package br.com.eduardo.drogaria.dao;
+
 import java.util.List;
 
 import org.hibernate.Session;
@@ -10,27 +11,32 @@ import br.com.eduardo.drogaria.domain.Venda;
 import br.com.eduardo.drogaria.util.HibernateUtil;
 
 public class VendaDAO extends GenericDAO<Venda> {
-	public void salvar(Venda venda, List<ItemVenda> itensVenda){
+	public void salvar(Venda venda, List<ItemVenda> itensVenda) {
 		Session sessao = HibernateUtil.getFabricaDeSessoes().openSession();
 		Transaction transacao = null;
 
 		try {
 			transacao = sessao.beginTransaction();
-		
+
 			sessao.save(venda);
-			
-			for(int posicao = 0; posicao < itensVenda.size(); posicao++){
+
+			for (int posicao = 0; posicao < itensVenda.size(); posicao++) {
 				ItemVenda itemVenda = itensVenda.get(posicao);
 				itemVenda.setVenda(venda);
-				
+
 				sessao.save(itemVenda);
-				
+
 				Produto produto = itemVenda.getProduto();
-				produto.setQuantidade(new Short((produto.getQuantidade() - itemVenda.getQuantidade()) + ""));
-				
-				sessao.update(produto);
+				int qtde = produto.getQuantidade() - itemVenda.getQuantidade();
+				if (qtde >= 0) {
+					produto.setQuantidade(new Short((produto.getQuantidade() - itemVenda.getQuantidade()) + ""));
+					sessao.update(produto);
+				} else {
+					throw new RuntimeException("Quantidade insuficiente em estoque");
+				}
+
 			}
-			
+
 			transacao.commit();
 		} catch (RuntimeException erro) {
 			if (transacao != null) {
