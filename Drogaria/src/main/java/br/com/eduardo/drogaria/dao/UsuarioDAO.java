7cf9c83d1bf0921d0a8b0151738de1a384e9930f
EduardoMRT <eduksoficial@gmail.com>
2024-03-01 08:58:40 -0300

    autenticar SRD pt2

--- Drogaria/src/main/java/br/com/eduardo/drogaria/dao/UsuarioDAO.java
+++ Drogaria/src/main/java/br/com/eduardo/drogaria/dao/UsuarioDAO.java
@@ -1,7 +1,28 @@
 package br.com.eduardo.drogaria.dao;
 
+import org.hibernate.Criteria;
+import org.hibernate.Session;
+import org.hibernate.criterion.Restrictions;
+
 import br.com.eduardo.drogaria.domain.Usuario;
+import br.com.eduardo.drogaria.util.HibernateUtil;
 
 public class UsuarioDAO extends GenericDAO<Usuario> {
+	public Usuario autenticar(String cpf, String senha) {
+		Session sessao = HibernateUtil.getFabricaDeSessoes().openSession();
 
+		try {
+			Criteria consulta = sessao.createCriteria(Usuario.class);
+
+			consulta.createAlias("pessoa", "p");
+
+			consulta.add(Restrictions.eq("p.cpf", cpf));
+			consulta.add(Restrictions.eq("senha", senha));
+
+			Usuario resultado = (Usuario) consulta.uniqueResult();
+			return resultado;
+		} catch (RuntimeException erro) {
+			throw erro;
+		}
+	}
 }
