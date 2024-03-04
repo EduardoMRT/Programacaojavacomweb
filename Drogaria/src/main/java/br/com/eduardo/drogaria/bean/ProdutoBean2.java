b38b8807b74b6e9eb4b3dc0357fc5d916bccc671
EduardoMRT <eduksoficial@gmail.com>
2024-03-04 10:34:55 -0300

    aula  286 - Cadastro de Produtos - View Param

--- Drogaria/src/main/java/br/com/eduardo/drogaria/bean/ProdutoBean2.java
+++ Drogaria/src/main/java/br/com/eduardo/drogaria/bean/ProdutoBean2.java
@@ -9,28 +9,32 @@ import javax.faces.bean.ViewScoped;
 
 import org.omnifaces.util.Messages;
 
+import br.com.eduardo.drogaria.dao.FabricanteDAO;
 import br.com.eduardo.drogaria.dao.ProdutoDAO;
+import br.com.eduardo.drogaria.domain.Fabricante;
 import br.com.eduardo.drogaria.domain.Produto;
+import lombok.Getter;
+import lombok.Setter;
 
 
 @SuppressWarnings("serial")
+@Getter
+@Setter
 @ManagedBean
 @ViewScoped
 public class ProdutoBean2 implements Serializable{
+	private Produto produto;
+	private Long codigoProduto;
+	
 	private List<Produto> produtos;
+	private List<Fabricante> fabricantes;
 	
+	private FabricanteDAO fabricanteDAO;
 	private ProdutoDAO produtoDAO;
-	
-	public List<Produto> getProdutos() {
-		return produtos;
-	}
-	
-	public void setProdutos(List<Produto> produtos) {
-		this.produtos = produtos;
-	}
 	
 	@PostConstruct
 	public void inicar() {
+		fabricanteDAO = new FabricanteDAO();
 		produtoDAO = new ProdutoDAO();
 	}
 	
@@ -39,6 +43,16 @@ public class ProdutoBean2 implements Serializable{
 			produtos = produtoDAO.listar("descricao");	
 		} catch (RuntimeException erro) {
 			Messages.addGlobalError("Ocorreu um erro ao tentar listar os produtos");
+			erro.printStackTrace();
+		}
+	}
+	
+	public void carregarEdicao() {
+		try {
+			produto = produtoDAO.buscar(codigoProduto);
+			fabricantes = fabricanteDAO.listar("descricao");
+		} catch (RuntimeException erro) {
+			Messages.addGlobalError("Ocorreu um merro ao tentar carregar os dados para edição");
 			erro.printStackTrace();
 		}
 	}
