b9e03754a1aff0eedc4a29b6990a8a77ff33ec96
EduardoMRT <eduksoficial@gmail.com>
2024-03-04 15:57:56 -0300

    Se der erro essa é a resolução
    https://stackoverflow.com/questions/19930241/how-to-inject-in-a-phaselistener

--- Drogaria/src/main/java/br/com/eduardo/drogaria/util/AutenticacaoListener.java
+++ Drogaria/src/main/java/br/com/eduardo/drogaria/util/AutenticacaoListener.java
@@ -1,5 +1,28 @@
 package br.com.eduardo.drogaria.util;
 
-public class AutenticacaoListener {
+import javax.faces.event.PhaseEvent;
+import javax.faces.event.PhaseId;
+import javax.faces.event.PhaseListener;
 
+import org.omnifaces.util.Faces;
+
+@SuppressWarnings("serial")
+public class AutenticacaoListener implements PhaseListener{
+
+	@Override
+	public void afterPhase(PhaseEvent event) {
+		Faces.getSessionAttribute(null);
+	}
+
+	@Override
+	public void beforePhase(PhaseEvent event) {
+		
+		
+	}
+
+	@Override
+	public PhaseId getPhaseId() {
+		return PhaseId.RESTORE_VIEW;
+	}
+	
 }
