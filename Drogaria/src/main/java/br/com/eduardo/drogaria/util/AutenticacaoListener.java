285332d7a28b4705e998643697f83b472ac40771
EduardoMRT <eduksoficial@gmail.com>
2024-03-05 08:28:51 -0300

    aula 292 - PhaseListener - Controle de Acesso

--- Drogaria/src/main/java/br/com/eduardo/drogaria/util/AutenticacaoListener.java
+++ Drogaria/src/main/java/br/com/eduardo/drogaria/util/AutenticacaoListener.java
@@ -6,23 +6,33 @@ import javax.faces.event.PhaseListener;
 
 import org.omnifaces.util.Faces;
 
+import br.com.eduardo.drogaria.bean.AutenticacaoBean;
+
 @SuppressWarnings("serial")
-public class AutenticacaoListener implements PhaseListener{
+public class AutenticacaoListener implements PhaseListener {
 
 	@Override
 	public void afterPhase(PhaseEvent event) {
-		Faces.getSessionAttribute(null);
+		
+		String paginaAtual = Faces.getViewId();
+		boolean ehPaginaDeAutenticacao = paginaAtual.contains("autenticacao.xhtml") ? true : false; 
+		if(ehPaginaDeAutenticacao == false) {
+			AutenticacaoBean autenticacaoBean = Faces.getSessionAttribute("autenticacaoBean");
+		
+			if(autenticacaoBean == null) {
+				Faces.navigate("/pages/autenticacao.xhtml");
+			}
+		}
 	}
 
 	@Override
 	public void beforePhase(PhaseEvent event) {
-		
-		
+
 	}
 
 	@Override
 	public PhaseId getPhaseId() {
 		return PhaseId.RESTORE_VIEW;
 	}
-	
+
 }
