b4fb7b6ce81e692e3bddad2e947827eff2f126c4
EduardoMRT <eduksoficial@gmail.com>
2024-03-07 11:57:01 -0300

    Envio de Email 100%

--- Drogaria/src/main/java/br/com/eduardo/drogaria/domain/CreateEmail.java
+++ Drogaria/src/main/java/br/com/eduardo/drogaria/domain/CreateEmail.java
@@ -3,29 +3,32 @@ package br.com.eduardo.drogaria.domain;
 import org.apache.commons.mail.DefaultAuthenticator;
 import org.apache.commons.mail.SimpleEmail;
 
-public class CreateEmail{
-	
-	public void email() {
-		String meuEmail = "desenvolvimentostestes@gmail.com";
-		String minhaSenha = "dgvtcyiyjwiwsykz";
-		
-		SimpleEmail email = new SimpleEmail();
-		email.setHostName("smtp.gmail.com");
-		email.setSmtpPort(587);
-		email.setAuthenticator(new DefaultAuthenticator(meuEmail, minhaSenha));
-		email.setSSLOnConnect(true);
-		
+import br.com.eduardo.drogaria.dao.EmailDAO;
+
+public class CreateEmail {
+
+	public void email(String destinatario, String assunto, String mensagem) {
+
 		try {
+			EmailDAO emailDAO = new EmailDAO();
+			Email email2 = new Email();
+			email2 = emailDAO.buscarPorCodigo(1);
 			
-			email.setFrom(meuEmail);
-			email.setSubject("Teste de Envio de Emails");
-			email.setMsg("Funcionou!");
-			email.addTo(meuEmail);
+			SimpleEmail email = new SimpleEmail();
+			email.setHostName("smtp.gmail.com");
+			email.setSmtpPort(587);
+			email.setAuthenticator(new DefaultAuthenticator(email2.getEmail(), email2.getSenha()));
+			email.setSSLOnConnect(true);
+			email.setFrom(email2.getEmail());
+			email.setSubject(assunto);
+			email.setMsg(mensagem);
+			email.addTo(destinatario);
 			email.send();
 			System.out.println("Email enviado com sucesso!");
-			
+
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
 	}
+
 }
--- Drogaria/src/test/java/br/com/eduardo/drogaria/domain/EnviaEmailTest.java
+++ Drogaria/src/test/java/br/com/eduardo/drogaria/domain/EnviaEmailTest.java
@@ -1,11 +1,13 @@
 package br.com.eduardo.drogaria.domain;
 
+import org.junit.Ignore;
 import org.junit.Test;
 
 public class EnviaEmailTest {
+	@Ignore
 	@Test
 	public void enviarEmail() {
 		CreateEmail createEmail = new CreateEmail();
-		createEmail.email();
+		createEmail.email("eduardoteixeira.dev@gmail.com", "Teste de Emails para outros usuarios", "Testando emails para outros usuarios");
 	}
 }
