125e1d63e5ade5dead803cb816945593ada017a1
EduardoMRT <eduksoficial@gmail.com>
2024-03-07 09:58:59 -0300

    Email funcionando

--- Drogaria/src/main/java/br/com/eduardo/drogaria/domain/CreateEmail.java
+++ Drogaria/src/main/java/br/com/eduardo/drogaria/domain/CreateEmail.java
@@ -1,38 +1,30 @@
 package br.com.eduardo.drogaria.domain;
 
-import javax.mail.*;
-import javax.mail.internet.InternetAddress;
-import javax.mail.internet.MimeMessage;
-import java.util.Properties;
+import org.apache.commons.mail.DefaultAuthenticator;
+import org.apache.commons.mail.SimpleEmail;
 
-public class CreateEmail {
-	
-    public void sendEmail(String to, String subject, String body) {
-        Properties props = new Properties();
-        props.put("mail.smtp.auth", "true");
-        props.put("mail.smtp.starttls.enable", "true");
-        props.put("mail.smtp.host", "smtp.gmail.com");
-        props.put("mail.smtp.port", "587");
-
-        // Configuração das propriedades (semelhante ao exemplo anterior)
-
-        Session session = Session.getInstance(props, new Authenticator() {
-            @Override
-            protected PasswordAuthentication getPasswordAuthentication() {
-                return new PasswordAuthentication("eduardoteixeira.dev@gmail.com", "AIzaSyBh9zl6K0py0yPyz9k7S6lRLUMOJoEDP9g");
-            }
-        });
-
-        try {
-            Message message = new MimeMessage(session);
-            message.setFrom(new InternetAddress("eduardoteixeira.dev@gmail.com"));
-            message.setRecipients(Message.RecipientType.TO, InternetAddress.parse(to));
-            message.setSubject(subject);
-            message.setText(body);
-
-            Transport.send(message);
-        } catch (MessagingException e) {
-            e.printStackTrace();
-        }
-    }
+public class CreateEmail{
+	public void email() {
+		String meuEmail = "desenvolvimentostestes@gmail.com";
+		String minhaSenha = "dgvtcyiyjwiwsykz";
+		
+		SimpleEmail email = new SimpleEmail();
+		email.setHostName("smtp.gmail.com");
+		email.setSmtpPort(587);
+		email.setAuthenticator(new DefaultAuthenticator(meuEmail, minhaSenha));
+		email.setSSLOnConnect(true);
+		
+		try {
+			
+			email.setFrom(meuEmail);
+			email.setSubject("Teste de Envio de Emails");
+			email.setMsg("Funcionou!");
+			email.addTo(meuEmail);
+			email.send();
+			System.out.println("Email enviado com sucesso!");
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
 }
--- Drogaria/src/test/java/br/com/eduardo/drogaria/domain/EnviaEmailTest.java
+++ Drogaria/src/test/java/br/com/eduardo/drogaria/domain/EnviaEmailTest.java
@@ -3,9 +3,9 @@ package br.com.eduardo.drogaria.domain;
 import org.junit.Test;
 
 public class EnviaEmailTest {
-	CreateEmail createEmail = new CreateEmail();
 	@Test
-	public void criar() {
-		createEmail.sendEmail("desenvolvimentostestes@gmail.com", "Teste", "Teste");
+	public void enviarEmail() {
+		CreateEmail createEmail = new CreateEmail();
+		createEmail.email();
 	}
 }
